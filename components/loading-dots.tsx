e83ee56b5a7019cdc59aec622125c3926d5ec380
oluoko <brianotieno586@gmail.com>
2025-06-07 15:38:59 +0300

    L.i.t.D.B: Updated loading-dots.tsx component

--- components/loading-dots.tsx
+++ components/loading-dots.tsx
@@ -5,8 +5,24 @@ import { useEffect, useState } from "react";
 interface LoadingDotsProps {
   text: string;
   dotNumber?: number;
+  size?:
+    | "xs"
+    | "sm"
+    | "base"
+    | "lg"
+    | "xl"
+    | "2xl"
+    | "3xl"
+    | "4xl"
+    | "5xl"
+    | "6xl";
 }
-export default function LoadingDots({ text, dotNumber = 4 }: LoadingDotsProps) {
+
+export default function LoadingDots({
+  text,
+  dotNumber = 4,
+  size = "base",
+}: LoadingDotsProps) {
   const [dots, setDots] = useState(1);
 
   useEffect(() => {
@@ -16,8 +32,23 @@ export default function LoadingDots({ text, dotNumber = 4 }: LoadingDotsProps) {
     return () => clearInterval(interval);
   }, [dotNumber]);
 
+  const sizeClasses = {
+    xs: "text-xs",
+    sm: "text-sm",
+    base: "text-base",
+    lg: "text-lg",
+    xl: "text-xl",
+    "2xl": "text-2xl",
+    "3xl": "text-3xl",
+    "4xl": "text-4xl",
+    "5xl": "text-5xl",
+    "6xl": "text-6xl",
+  };
+
   return (
-    <span className="inline-block animate-pulse font-bold">
+    <span
+      className={`inline-block animate-pulse font-bold ${sizeClasses[size]}`}
+    >
       {text}
       {".".repeat(dots)}
     </span>
